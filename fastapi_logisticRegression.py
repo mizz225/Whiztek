b64cf1eb06d4ad483d33b642c1abac76568bd026
mizz225 <muzammilimran225@gmail.com>
2025-03-11 08:26:53 +0530

    Added few more relevant files and made changes in few other files

--- fastapi_logisticRegression.py
+++ fastapi_logisticRegression.py
@@ -33,10 +33,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all HTTP methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"], 
+    allow_headers=["*"],
 )
 
 @app.get("/", response_class=HTMLResponse)
